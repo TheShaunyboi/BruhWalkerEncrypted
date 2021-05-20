c2a165563058d112efa60ccf6d602b330fd4c5b8
TheShaunyboi <82087018+TheShaunyboi@users.noreply.github.com>
2021-05-20 18:32:23 +0100

    Update VIP_USER_LIST.lua

--- VIP_USER_LIST.lua
+++ VIP_USER_LIST.lua
@@ -1,11 +1,4 @@
-do
-  VIP_users = {"bensto23", "shaunyboi"}
-  local function VIP_USER_LIST()
-    local USER = client.username
-    for _, value in ipairs(VIP_users) do
-      if value == USER then
-        return true
-      end
-    return false
-  VIP_USER_LIST()
-end
+VIP_USER_LIST.lua
+shaunyboi
+bensto23
+lucifersAngel
