7f65bd35ccfad845157ab77dd9f53d420603f65a
TheShaunyboi <82087018+TheShaunyboi@users.noreply.github.com>
2022-02-27 10:25:13 +0000

    Update Shaunyboi-RandomUtilities.lua

--- Shaunyboi-RandomUtilities.lua
+++ Shaunyboi-RandomUtilities.lua
@@ -1,27 +1,26 @@
-
+local UpdateDraw = false
 do
-  local function AutoUpdate()
-	local Version = 0.1
-	local file_name = "Shaunyboi-RandomUtilities.lua"
-	local url = "https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Shaunyboi-RandomUtilities.lua"
-  local web_version = http:get("https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Shaunyboi-RandomUtilities.lua.version.txt")
-	if tonumber(web_version) == Version then
-    console:log("Shauny's Random Utilities Successfully Loaded......")
-  else
-		http:download_file(url, file_name)
-    console:log("Shaunyboi-RandomUtilities.lua Vers: "..Version)
-  	console:log("Shaunyboi-RandomUtilities.Web Vers: "..tonumber(web_version))
-		console:log("Shaunyboi-Random Utilities Update Available.....")
-		console:log("Please Reload via F5!............")
-		console:log("---------------------------------")
-		console:log("Please Reload via F5!............")
-    console:log("---------------------------------")
-  end
-  end
+  	local function AutoUpdate()
+		local Version = 0.2
+		local file_name = "Shaunyboi-RandomUtilities.lua"
+		local url = "https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Shaunyboi-RandomUtilities.lua"
+		local web_version = http:get("https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Shaunyboi-RandomUtilities.lua.version.txt")
+		console:log("Shaunyboi-RandomUtilities.lua Vers: "..Version)
+		console:log("Shaunyboi-RandomUtilities.Web Vers: "..tonumber(web_version))
+		if tonumber(web_version) == Version then
+			console:log("Shauny's Utilities Successfully Loaded...")
+		else
+			http:download_file(url, file_name)
+			console:log("Shaunyboi-Random Utilities Update Available...")
+			console:log("Please Reload via F5!...")	
+			UpdateDraw = true
+		end	
+  	end
+
   AutoUpdate()
 end
 
---Ensuring that the librarys are downloaded:
+--Ensuring that the librarys and sound files are downloaded:
 local file_name = "VectorMath.lua"
 if not file_manager:file_exists(file_name) then
    local url = "https://raw.githubusercontent.com/stoneb2/Bruhwalker/main/VectorMath/VectorMath.lua"
@@ -66,10 +65,12 @@ if not file_manager:file_exists("Shaun's Sexy Common//MalePentaKillSound.wav") t
 	http:download_file("https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Common/MalePentaKillSound.wav", "Shaun's Sexy Common//MalePentaKillSound.wav")
 end
 
+local SoundsDownloaded = false
 if not file_manager:file_exists("Shaun's Sexy Common//FemalePentaKillSound.wav") then
 	http:download_file("https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Common/FemalePentaKillSound.wav", "Shaun's Sexy Common//FemalePentaKillSound.wav")
 	console:log("--- ALL SOUNDS ARE DOWNLOADED PLEASE PRESS F5--- ")
 	console:log("--- LOVE YOU ALL <3 ---")
+	SoundsDownloaded = true
 end
 
 
@@ -113,14 +114,19 @@ random_enabled = menu:add_checkbox("Enabled", random_category, 1)
 menu:add_label("Shauny's Random Utilities", random_category)
 menu:add_label("#Loveyou", random_category)
 
-vision_wards = menu:add_subcategory("[Ward On Vision Feature Lost Settings]", random_category)
-random_no_vision = menu:add_checkbox("Use [Ward] On Vision Lost", vision_wards, 0)
+thresh_grab = menu:add_subcategory("[Auto Thresh Lantern Features]", random_category)
+thresh_lantern_key = menu:add_keybinder("Auto Lantern Grab Key ", thresh_grab, 88)
+thresh_auto_ward = menu:add_checkbox("Use [Wards] On Enemy Thresh Lantern", thresh_grab, 1)
+
+vision_wards = menu:add_subcategory("[Auto Ward On Vision Lost Settings]", random_category)
+menu:add_label("Vision Lost In All Grass Area's While Holding Combo Key", vision_wards)
+random_no_vision = menu:add_checkbox("Use [Ward] On Vision Lost", vision_wards, 1)
 random_no_vision_blue = menu:add_checkbox("Use [Blue Ward] On Vision Lost", vision_wards, 1)
-random_no_vision_yellow = menu:add_checkbox("Use [Blue Ward] On Vision Lost", vision_wards, 1)
+random_no_vision_yellow = menu:add_checkbox("Use [Yellow Ward] On Vision Lost", vision_wards, 1)
 random_no_vision_control = menu:add_checkbox("Use [Control Ward] On Vision Lost", vision_wards, 1)
 
 sounds_selector = menu:add_subcategory("[Kill Sounds Settings]", random_category)
-sounds_selector_use = menu:add_checkbox("Use [Kill Sounds]", sounds_selector, 0)
+sounds_selector_use = menu:add_checkbox("Use [Kill Sounds]", sounds_selector, 1)
 sounds_selector_1 = menu:add_subcategory("[First Kill Sound] Settings", sounds_selector)
 a_table = {}
 a_table[1] = "Quake Male Voice"
@@ -256,7 +262,6 @@ local function on_kda_updated(kill, death, assist)
   end
 end
 
------------------------------------------------------------------------------------
 -- on_lose_vision --
 
 local function BlueWardCheck()
@@ -283,50 +288,46 @@ local function ControlWardCheck()
   local control_ward = false
   local control_ward_slot = nil
   local inventory = ml.GetItems()
-  for _, v in ipairs(inventory) do
-    if tonumber(v) == 2055 then
-    	local item = local_player:get_item(tonumber(v))
-    	if item ~= 0 then
-		    control_ward_slot = ml.SlotSet("SLOT_ITEM"..tostring(item.slot))
+  	for _, v in ipairs(inventory) do
+		if tonumber(v) == 2055 then
+			local item = local_player:get_item(tonumber(v))
+			if item ~= 0 then
+				control_ward_slot = ml.SlotSet("SLOT_ITEM"..tostring(item.slot))
 				if ml.Ready(control_ward_slot) then
 					control_ward = true
 				end
 			end
+		end
   	end
-  end
   return control_ward, control_ward_slot
 end
 
 local function on_lose_vision(obj)
 
-  if menu:get_value(random_enabled) == 1 and menu:get_value(random_no_vision) == 1 then
+	if menu:get_value(random_enabled) == 1 and menu:get_value(random_no_vision) == 1 then
+		if nav_mesh:is_grass(obj.origin.x, obj.origin.y, obj.origin.z) and combo:get_mode() == 1 or combo:get_mode() == MODE_COMBO then
+			
+			local control_ward, control_ward_slot = ControlWardCheck()
 
-    local control_ward, control_ward_slot = ControlWardCheck()
-
-  	if menu:get_value(random_no_vision_blue) == 1 and myHero:distance_to(obj.origin) <= 4000 then
-  		if BlueWardCheck() then
-  			origin = obj.origin
-  			x, y, z = origin.x, origin.y, origin.z
-  			spellbook:cast_spell(SLOT_WARD, 0.5, x, y, z)
-  		end
-    end
-    if menu:get_value(random_no_vision_yellow) == 1 and myHero:distance_to(obj.origin) <= 600 then
-  		if YellowWardCheck() then
-  			origin = obj.origin
-  			x, y, z = origin.x, origin.y, origin.z
-  			spellbook:cast_spell(SLOT_WARD, 0.5, x, y, z)
-  		end
-    end
-    if menu:get_value(random_no_vision_control) == 1 and myHero:distance_to(obj.origin) <= 600 then
-  		if not YellowWardCheck() and not BlueWardCheck() and control_ward then
-  			if ml.Ready(control_ward_slot) then
-  				origin = obj.origin
-  				x, y, z = origin.x, origin.y, origin.z
-  				spellbook:cast_spell(control_ward_slot, 0.5, x, y, z)
-  			end
-  		end
-  	end
-  end
+			if menu:get_value(random_no_vision_blue) == 1 and myHero:distance_to(obj.origin) <= 1000 then
+				if BlueWardCheck() then
+					spellbook:cast_spell(SLOT_WARD, 0.5, obj.origin.x, obj.origin.y, obj.origin.z)
+				end
+			end
+			if menu:get_value(random_no_vision_yellow) == 1 and myHero:distance_to(obj.origin) <= 600 then
+				if YellowWardCheck() then
+					spellbook:cast_spell(SLOT_WARD, 0.5, obj.origin.x, obj.origin.y, obj.origin.z)
+				end
+			end
+			if menu:get_value(random_no_vision_control) == 1 and myHero:distance_to(obj.origin) <= 600 then
+				if not YellowWardCheck() and not BlueWardCheck() and control_ward then
+					if ml.Ready(control_ward_slot) then
+						spellbook:cast_spell(control_ward_slot, 0.5, obj.origin.x, obj.origin.y, obj.origin.z)
+					end
+				end
+			end
+		end
+	end	
 end
 
 -----------------------------------------------------------------------------------
@@ -342,107 +343,140 @@ local function on_draw()
 		x, y, z = origin.x, origin.y, origin.z
 	end
 
-  if menu:get_value(random_enabled) == 1 and menu:get_value(sounds_selector_use) == 1 then
+  	if menu:get_value(random_enabled) == 1 and menu:get_value(sounds_selector_use) == 1 then
 
-  	if menu:get_value(draw_killreset) == 1 and kill_1 and not kill_2 then
-  	 	if myHero.is_on_screen and endTime_kill_1 ~= nil then
-  			local countdown = endTime_kill_1 - os.time()
-  			renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Double Kill Reset Time")
-  		 	renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
-  	 	end
-   	end
-  	if menu:get_value(draw_killreset) == 1 and kill_2 and not kill_3 then
-  	 	if myHero.is_on_screen and endTime_kill_2 ~= nil then
-  			local countdown = endTime_kill_2 - os.time()
-  			renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Triple Kill Reset Time")
-  		 	renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
-  	 	end
-   	end
-  	if menu:get_value(draw_killreset) == 1 and kill_3 and not kill_4 then
-  	 	if myHero.is_on_screen and endTime_kill_3 ~= nil then
-  			local countdown = endTime_kill_3 - os.time()
-  			renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Quadra Kill Reset Time")
-  		 	renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
-  	 	end
-   	end
-  	if menu:get_value(draw_killreset) == 1 and kill_4 then
-  	 	if myHero.is_on_screen and endTime_kill_4 ~= nil then
-  			local countdown = endTime_kill_4 - os.time()
-  			renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Penta Kill Reset Time")
-  		 	renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
-  	 	end
-   	end
-  end
+		if menu:get_value(draw_killreset) == 1 and kill_1 and not kill_2 then
+			if myHero.is_on_screen and endTime_kill_1 ~= nil then
+				local countdown = endTime_kill_1 - os.time()
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Double Kill Reset Time")
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
+			end
+		end
+		if menu:get_value(draw_killreset) == 1 and kill_2 and not kill_3 then
+			if myHero.is_on_screen and endTime_kill_2 ~= nil then
+				local countdown = endTime_kill_2 - os.time()
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Triple Kill Reset Time")
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
+			end
+		end
+		if menu:get_value(draw_killreset) == 1 and kill_3 and not kill_4 then
+			if myHero.is_on_screen and endTime_kill_3 ~= nil then
+				local countdown = endTime_kill_3 - os.time()
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Quadra Kill Reset Time")
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
+			end
+		end
+		if menu:get_value(draw_killreset) == 1 and kill_4 then
+			if myHero.is_on_screen and endTime_kill_4 ~= nil then
+				local countdown = endTime_kill_4 - os.time()
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 40, "Penta Kill Reset Time")
+				renderer:draw_text_big_centered(myherodraw.x, myherodraw.y + 80, tostring(countdown))
+			end
+		end
+  	end
+
+	if UpdateDraw then
+		renderer:draw_text_big_centered(screen_size.width / 2, screen_size.height / 2, "Shaun's Utilities Update Available... Press F5")
+	end	
+	if SoundsDownloaded then
+		renderer:draw_text_big_centered(screen_size.width / 2, screen_size.height / 2 + 50, "All Quake Kill Sounds Downloaded... Press F5")
+	end	
 end
 
 local function on_tick()
 
-  if menu:get_value(random_enabled) == 1 and menu:get_value(sounds_selector_use) == 1 then
+  	if menu:get_value(random_enabled) == 1 and menu:get_value(sounds_selector_use) == 1 then
 
-  	if kill_1 and endTime_kill_1 ~= nil then
-  		if os.time() > endTime_kill_1 then
-  			if not kill_2 and not kill_3 and not kill_4 and not kill_5 then
-  				kill_5 = false
-  				kill_4 = false
-  				kill_3 = false
-  				kill_2 = false
-  				kill_1 = false
-  				endTime_kill_1 = nil
-  				endTime_kill_2 = nil
-  				endTime_kill_3 = nil
-  				endTime_kill_4 = nil
-  			end
-  		end
-  	end
+		if kill_1 and endTime_kill_1 ~= nil then
+			if os.time() > endTime_kill_1 then
+				if not kill_2 and not kill_3 and not kill_4 and not kill_5 then
+					kill_5 = false
+					kill_4 = false
+					kill_3 = false
+					kill_2 = false
+					kill_1 = false
+					endTime_kill_1 = nil
+					endTime_kill_2 = nil
+					endTime_kill_3 = nil
+					endTime_kill_4 = nil
+				end
+			end
+		end
 
-  	if kill_2 and endTime_kill_2 ~= nil then
-  		if os.time() > endTime_kill_2 then
-  			if not kill_3 and not kill_4 and not kill_5 then
-  				kill_5 = false
-  				kill_4 = false
-  				kill_3 = false
-  				kill_2 = false
-  				kill_1 = false
-  				endTime_kill_1 = nil
-  				endTime_kill_2 = nil
-  				endTime_kill_3 = nil
-  				endTime_kill_4 = nil
-  			end
-  		end
-  	end
+		if kill_2 and endTime_kill_2 ~= nil then
+			if os.time() > endTime_kill_2 then
+				if not kill_3 and not kill_4 and not kill_5 then
+					kill_5 = false
+					kill_4 = false
+					kill_3 = false
+					kill_2 = false
+					kill_1 = false
+					endTime_kill_1 = nil
+					endTime_kill_2 = nil
+					endTime_kill_3 = nil
+					endTime_kill_4 = nil
+				end
+			end
+		end
 
-  	if kill_3 and endTime_kill_3 ~= nil then
-  		if os.time() > endTime_kill_3 then
-  			if not kill_4 and not kill_5 then
-  				kill_5 = false
-  				kill_4 = false
-  				kill_3 = false
-  				kill_2 = false
-  				kill_1 = false
-  				endTime_kill_1 = nil
-  				endTime_kill_2 = nil
-  				endTime_kill_3 = nil
-  				endTime_kill_4 = nil
-  			end
-  		end
-  	end
+		if kill_3 and endTime_kill_3 ~= nil then
+			if os.time() > endTime_kill_3 then
+				if not kill_4 and not kill_5 then
+					kill_5 = false
+					kill_4 = false
+					kill_3 = false
+					kill_2 = false
+					kill_1 = false
+					endTime_kill_1 = nil
+					endTime_kill_2 = nil
+					endTime_kill_3 = nil
+					endTime_kill_4 = nil
+				end
+			end
+		end
 
-  	if kill_4 and endTime_kill_4 ~= nil then
-  		if os.time() > endTime_kill_4 then
-  			if not kill_5 then
-  				kill_5 = false
-  				kill_4 = false
-  				kill_3 = false
-  				kill_2 = false
-  				kill_1 = false
-  				endTime_kill_1 = nil
-  				endTime_kill_2 = nil
-  				endTime_kill_3 = nil
-  				endTime_kill_4 = nil
-  			end
-  		end
-  	end
-  end
+		if kill_4 and endTime_kill_4 ~= nil then
+			if os.time() > endTime_kill_4 then
+				if not kill_5 then
+					kill_5 = false
+					kill_4 = false
+					kill_3 = false
+					kill_2 = false
+					kill_1 = false
+					endTime_kill_1 = nil
+					endTime_kill_2 = nil
+					endTime_kill_3 = nil
+					endTime_kill_4 = nil
+				end
+			end
+		end
+
+
+		if game:is_key_down(menu:get_value(thresh_lantern_key)) then
+			allypets = game.pets
+			for _, allyminion in ipairs(allypets) do
+				if not allyminion.is_enemy and allyminion:distance_to(myHero.origin) <= myHero.attack_range and allyminion.object_name == "ThreshLantern" then
+					spellbook:cast_spell_targetted(62, allyminion, 0.25)
+				end
+			end	
+		end	
+
+		if menu:get_value(thresh_auto_ward) == 1 then
+			local control_ward, control_ward_slot = ControlWardCheck()
+			pets = game.pets
+			for _, minion in ipairs(pets) do
+				if minion.is_enemy and minion:distance_to(myHero.origin) <= 600 and minion.object_name == "ThreshLantern" then
+					if BlueWardCheck() or YellowWardCheck() then
+						spellbook:cast_spell(SLOT_WARD, 0.5, minion.origin.x, minion.origin.y, minion.origin.z)
+					else 
+						if control_ward and ml.Ready(control_ward_slot) then
+							spellbook:cast_spell(control_ward_slot, 0.5, minion.origin.x, minion.origin.y, minion.origin.z)
+						end
+					end	
+				end
+			end	
+		end	
+	end
 end
 
 client:set_event_callback("on_tick", on_tick)
