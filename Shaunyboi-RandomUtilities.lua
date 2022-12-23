4db2240b0bf93274acdfc840de90af3d801f752a
TheShaunyboi <82087018+TheShaunyboi@users.noreply.github.com>
2022-12-23 14:43:49 +0000

    Update Shaunyboi-RandomUtilities.lua

--- Shaunyboi-RandomUtilities.lua
+++ Shaunyboi-RandomUtilities.lua
@@ -1,7 +1,7 @@
 local UpdateDraw = false
 do
   	local function AutoUpdate()
-		local Version = 2.1
+		local Version = 2.3
 		local file_name = "Shaunyboi-RandomUtilities.lua"
 		local url = "https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Shaunyboi-RandomUtilities.lua"
 		local web_version = http:get("https://raw.githubusercontent.com/TheShaunyboi/BruhWalkerEncrypted/main/Shaunyboi-RandomUtilities.lua.version.txt")
@@ -312,6 +312,7 @@ menu:add_label("Only You Can See The Chat Print Output", tp_tracker)
 recall_tracker = menu:add_checkbox("Use Tracker", tp_tracker, 1) 
 rolling_banner = menu:add_checkbox("Notify With Rolling Banner", tp_tracker, 1) 
 recall_tracker_ping = menu:add_checkbox("FOW Ping Recall Start - Your Team Can See This", tp_tracker, 0) 
+recall_drawline = menu:add_checkbox("Draw Line To FOW Recalls", tp_tracker, 1) 
 
 sounds_selector = menu:add_subcategory("[Kill Sounds Settings]", random_category)
 sounds_selector_use = menu:add_checkbox("Use [Kill Sounds]", sounds_selector, 1)
@@ -570,6 +571,8 @@ local function AutoBuyWard()
 	end
 end
 
+local enemyPos = nil
+local line_name = nil
 local function on_teleport(obj, tp_duration, tp_name, status)
 	
 	if menu:get_value(recall_tracker) == 1 and obj.is_enemy then
@@ -583,15 +586,27 @@ local function on_teleport(obj, tp_duration, tp_name, status)
 				if menu:get_value(rolling_banner) == 1 then
 					notify:add(obj.champ_name .. " Recall Started", 255, 255, 255)
 				end
+				if menu:get_value(recall_drawline) == 1 and not obj.is_visible then
+					enemyPos = vec3.new(obj.origin.x, obj.origin.y, obj.origin.z)
+					line_name = obj
+				end
 			elseif status == "Finish" then
 				game:print_chat(obj.champ_name ..  " - <font color=\"#11ff00\"><b>Recall Complete</b></font><font")
 				if menu:get_value(rolling_banner) == 1 then
 					notify:add(obj.champ_name .. " Recall Complete", 255, 255, 255)
 				end
+				if obj == line_name then
+					enemyPos = nil
+					line_name = nil
+				end
 			elseif status == "Abort" then
 				game:print_chat(obj.champ_name ..  " - <font color=\"#ff0000\"><b>Recall Aborted</b></font><font")
 				if menu:get_value(rolling_banner) == 1 then
 					notify:add(obj.champ_name .. " Recall Aborted", 255, 255, 255)
+				end
+				if obj == line_name then
+					enemyPos = nil
+					line_name = nil
 				end
 			end
 		end
@@ -808,6 +823,12 @@ local function on_draw()
 				end
 			end
 		end
+	end
+
+	if line_name and enemyPos then
+		myPos = game:world_to_screen_2(myHero.origin.x, myHero.origin.y, myHero.origin.z)
+		b = game:world_to_screen_2(enemyPos.x, enemyPos.y, enemyPos.z)
+		renderer:draw_line(myPos.x, myPos.y, b.x, b.y, 2, 255, 0, 0, 255)
 	end
 end
 
